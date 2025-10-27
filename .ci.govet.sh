8150461b1baeb6e9f646cbf567f7c0a6f0adac34
Uwe Jugel <uwe@upvest.co>
2025-10-27 10:36:13 +0100

    remove elaboration in readme, remove testing.T tests

--- .ci.govet.sh
+++ .ci.govet.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-# Run go vet on all packages. To exclude specific tests that are known to
-# trigger vet warnings, use the 'novet' build tag. This is used in the
-# following tests:
-#
-# require/requirements_testing_test.go:
-#
-#    The 'testing' tests test testify behavior 😜 against a real testing.T,
-#    running tests in goroutines to capture Goexit behavior.
-#    Such usage triggers would normally trigger vet warnings (SA2002).
-
 go vet -tags novet ./...
