09241bb9a1e78eb38025d7f4b9a3f9579445a12f
Uwe Jugel <uwe@upvest.co>
2025-10-27 10:26:56 +0100

    undo mock test change

--- mock/mock_test.go
+++ mock/mock_test.go
@@ -1342,11 +1342,7 @@ func Test_Mock_Called(t *testing.T) {
 }
 
 func asyncCall(m *Mock, ch chan Arguments) {
-	var args Arguments
-	defer func() {
-		ch <- args
-	}()
-	args = m.Called(1, 2, 3)
+	ch <- m.Called(1, 2, 3)
 }
 
 func Test_Mock_Called_blocks(t *testing.T) {
